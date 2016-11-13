b95371c2c117479f34b6241e78a79ef76ab705b5
ddeep <tuladhar.d@gmail.com>
2016-11-13 11:12:28 +0545

    added side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,40 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component , ViewChild} from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
+import { DetailPage} from '../pages/detail/detail';
+import { ViewDataPage} from '../pages/view-data/view-data';
 
 
 @Component({
-  template: `<ion-nav [root]="rootPage"></ion-nav>`
+  templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = HomePage;
+  @ViewChild(Nav) nav: Nav;
 
-  constructor(platform: Platform) {
-    platform.ready().then(() => {
+  rootPage = HomePage;
+  pages: Array<{title: string, component: any}>;
+  constructor(public platform: Platform) {
+    this.pages = [
+      {title: 'Home',component: HomePage},
+      {title: 'Collect Data',component: DetailPage},
+      {title: 'View Data',component: ViewDataPage}
+
+    ];
+  }
+
+  initializeApp(){
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
+  }
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
   }
 }
